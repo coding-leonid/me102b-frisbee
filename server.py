0d44b7118bc33ff64eb7a06ce97e35bb2f53dfb7
leopot577 <pototskiy.leonid@gmail.com>
2024-04-02 15:44:54 -0700

    Second back-and-forth prototype

--- server.py
+++ server.py
@@ -8,24 +8,23 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator
 
-sender_socket = socket.socket()
-sender_socket.bind(('2607:f140:400:11e:7f37:e85f:5e9c:68a5', 8000))
-sender_socket.listen(0)
+server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+server_socket.bind(('::1', 8000))  # Use the IPv6 loopback address
+server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
-sender_connection = sender_socket.accept()[0].makefile('rb')
-
-# Create a socket for sending data back to the client
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('2607:f140:400:108:5598:8aa3:7974:81ac', 8001))
-
+connection = server_socket.accept()[0].makefile('rb')
 
 model = YOLO('yolov8n.pt')
+
+# Create a socket for sending data back to the client
+client_send_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+client_send_socket.connect(('::1', 8001))  # Use the IPv6 loopback address and the port of the client
 try:
     img = None
     while True:
         # Read the length of the image as a 32-bit unsigned int
-        image_len = struct.unpack('<L', sender_connection.read(struct.calcsize('<L')))[0]
+        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         #print(f'Read size: {image_len}')
         # If the length is zero, quit the loop
         if not image_len:
@@ -35,7 +34,7 @@ try:
         # Construct a stream to hold the image data and read the image
         # data from the connection
         image_stream = io.BytesIO()
-        image_stream.write(sender_connection.read(image_len))
+        image_stream.write(connection.read(image_len))
         # Rewind the stream and decode the image data
         image_stream.seek(0)
         data = np.frombuffer(image_stream.getvalue(), dtype=np.uint8)
@@ -59,7 +58,7 @@ try:
             
             # No control unless exactly one person in frame
             if len(track_ids) == 1:
-                client_socket.sendall(str(center_x).encode())
+                client_send_socket.sendall(str(center_x).encode())
 
         cv2.imshow('Person tracker', image)
 
@@ -71,6 +70,6 @@ except Exception as e:
     print(f'Exception caught: {e}')
 
 finally:
-    sender_connection.close()
-    sender_socket.close()
+    connection.close()
+    server_socket.close()
     print('Cleanup successful')
